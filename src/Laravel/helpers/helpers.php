c141083a919769ad39295940bf0691321e19aca2
mojtaba shafizadeh <mojtaba031@yahoo.com>
2022-07-23 00:17:44 +0430

    Delete src directory

--- src/Laravel/helpers/helpers.php
+++
@@ -1,8 +0,0 @@
-<?php
-    if (! function_exists('esraContent')) {
-        function esraContent( $type = "rating", $id = 3 )
-        {
-            return \Morilog\Jalali\Jalalian::forge($str)->format($format);
-        }
-    }
-?>
